9597be1b74c8b05221c096dd24852f99d5e6d382
Victor Manuel Villamil Romero <n10080236@qut.edu.au>
2019-02-27 11:28:41 +1000

    Second Commit project

--- src/main/OpenJumpRoadMatch.java
+++ src/main/OpenJumpRoadMatch.java
@@ -15,32 +15,56 @@ import com.vividsolutions.jump.task.TaskMonitor;
 import com.vividsolutions.jump.workbench.ui.GUIUtil;
 
 public class OpenJumpRoadMatch {
+	
+	public Shapefile dataSourceClass = null;
+	public static File file = new File("D:\\WIP\\Project Roadmatcher\\Java\\data\\sample\\dataset1.shp");
+	
 
 	public static void main(String[] args) {
 		//DataSource
-        File file = new File("D:\\WIP\\Project Roadmatcher\\Java\\data\\sample\\dataset1.shp");
-        Shapefile dataSourceClass = new Shapefile();
-        DataSource dataSource = (DataSource) LangUtil.newInstance(dataSourceClass.getClass());          
-        
+		DataSource dataSource = getDataSource();                
+
         //Datasource query.
-        Map<String, Object> options = new HashMap<String, Object>();
+		DataSourceQuery dataSourceQuery = getDataSourceQuery(dataSource);               
+        
+        // feature collection.      
+		FeatureCollection featureCollection = getFeatureCollection(dataSourceQuery);
+		
+		//Print Results
+		printResults( featureCollection);
+	}
+	
+	public static DataSource getDataSource() {		
+        Shapefile dataSourceClass = new Shapefile();
+		DataSource dataSource = (DataSource) LangUtil.newInstance(dataSourceClass.getClass()); 
+		
+		return dataSource;
+	}
+	
+	public static DataSourceQuery getDataSourceQuery(DataSource dataSource) {		
+		Map<String, Object> options = new HashMap<String, Object>();
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(DataSource.FILE_KEY, file.getPath());
         properties.put(DataSource.COORDINATE_SYSTEM_KEY, CoordinateSystem.UNSPECIFIED);
         properties.putAll(options);
         dataSource.setProperties(properties);       
         String layerName = GUIUtil.nameWithoutExtension(file) ;
-        DataSourceQuery dataSourceQuery = new DataSourceQuery(dataSource, null, layerName);        
+        DataSourceQuery dataSourceQuery = new DataSourceQuery(dataSource, null, layerName); 
         
-        // feature collection.      
-        Connection connection = dataSourceQuery.getDataSource().getConnection();
+		return dataSourceQuery;
+	}
+	
+	public static FeatureCollection getFeatureCollection(DataSourceQuery dataSourceQuery) {
+		Connection connection = dataSourceQuery.getDataSource().getConnection();
         ArrayList queryExceptions  = new ArrayList();
         TaskMonitor taskMonitor =  null; 
         FeatureCollection featureCollection  = connection.executeQuery(dataSourceQuery.getQuery(), queryExceptions, taskMonitor);
-        System.out.println(featureCollection);
-        System.out.println(featureCollection.getFeatures().size());
-        System.out.println(featureCollection.getFeatureSchema().getCoordinateSystem());
-
+		return featureCollection;
 	}
 
+	public static void printResults(FeatureCollection featureCollection) {
+		System.out.println(featureCollection);
+        System.out.println(featureCollection.getFeatures().size());
+        System.out.println(featureCollection.getFeatureSchema().getCoordinateSystem());
+	}
 }
